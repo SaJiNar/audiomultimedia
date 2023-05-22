27e2350d5d7d97b03c6a6051bc60b52237c275b8
SaJiNar <sarajnares@gmail.com>
2023-05-22 17:00:54 +0200

    Adding MainActivity

--- app/src/main/java/com/example/audiomultimedia/MainActivity.java
+++ app/src/main/java/com/example/audiomultimedia/MainActivity.java
@@ -3,16 +3,53 @@ package com.example.audiomultimedia;
 import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import java.io.IOException;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+    private Button btnPlay;
+    private Button btnPause;
+    private Button btnStop;
+    private MediaPlayer mediaplayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //Añadir aqui la cancion deseada
         MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.songtoplay);
-        mediaPlayer.start();
+
+        //Botones para la interfaz
+        btnPlay = (Button) findViewById(R.id.buttonPlay);
+        btnPause = (Button) findViewById(R.id.buttonStop);
+        btnStop = (Button) findViewById(R.id.buttonStop);
+
+        //asignar controlador de eventos
+        btnPlay.setOnClickListener(this);
+        btnPause.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonPlay:
+                mediaplayer.start();
+                break;
+            case R.id.buttonPause:
+                mediaplayer.pause();
+                break;
+            case R.id.buttonStop:
+                try{
+                    mediaplayer.stop();
+                    mediaplayer.prepare();
+                    mediaplayer.seekTo(0);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
     }
 }
